0cc69d5c2778048f7f7497da8f143fa4ad30c968
Arunabh Das <arunabhdas@gmail.com>
2019-11-21 12:13:19 -0500

    Added INVocabulary strings

--- finalfrontier-ios/ProjectX/AppDelegate.swift
+++ finalfrontier-ios/ProjectX/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        INVocabulary.shared().setVocabularyStrings(["push up", "sit up", "pull up"],  of: .workoutActivityName)
         requestAuthorisation()
         return true
     }
